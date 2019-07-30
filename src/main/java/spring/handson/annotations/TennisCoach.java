9a1f0d8b998f9cd030deaabfa19e56002a84f428
Rutva_Rajdev <rutva.rajdev@gmail.com>
2019-07-30 15:45:24 -0400

    Tested field injection using autowiring

--- src/main/java/spring/handson/annotations/TennisCoach.java
+++ src/main/java/spring/handson/annotations/TennisCoach.java
@@ -5,17 +5,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+
+    @Autowired
     private FortuneService fortuneService;
 
     public TennisCoach() {
         System.out.println("In Tennis coach default constructor");
     }
 
-    @Autowired
-    public void doSomething(FortuneService myFortuneService) {
-        System.out.println("Inside doSomething method tennis coach");
-        fortuneService = myFortuneService;
-    }
+//    @Autowired
+//    public void doSomething(FortuneService myFortuneService) {
+//        System.out.println("Inside doSomething method tennis coach");
+//        fortuneService = myFortuneService;
+//    }
 
 //    @Autowired
 //    public TennisCoach(FortuneService myFortuneService){
