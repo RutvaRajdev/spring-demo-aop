a180ea4599980c5ef4c6e9a5e10d2ede4549c35d
Rutva_Rajdev <rutva.rajdev@gmail.com>
2019-08-01 10:44:53 -0400

    Added spring configuration using Java file

---
+++ src/main/java/spring/handson/annotations/JavaConfigDemoApp.java
@@ -0,0 +1,25 @@
+package spring.handson.annotations;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class JavaConfigDemoApp {
+
+    public static void main(String[] args) throws IOException {
+        // Read spring config file
+        AnnotationConfigApplicationContext context = new
+                AnnotationConfigApplicationContext(SportConfig.class);
+
+        // Get the bean from container
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        // Call a method on bean
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+
+        // Close context
+        context.close();
+    }
+}
---
+++ src/main/java/spring/handson/annotations/SportConfig.java
@@ -0,0 +1,9 @@
+package spring.handson.annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spring.handson.annotations")
+public class SportConfig {
+}
